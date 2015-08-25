14f347fa9ac86a9f0279048b1fc10578ae7e156c
Simon Wörner <mail@simon-woerner.de>
2015-08-26 00:28:40 +0200

    use platform cert
    Change-Id: I60e7e75c68226af46d9579378f87be467d588cdf

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_PACKAGE_NAME := OpenDelta
 LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
